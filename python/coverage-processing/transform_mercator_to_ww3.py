1196ae99070f7940167eeedbaee052efd81b6078
Fabien Rétif <fabien.retif@zoho.com>
2016-03-16 13:47:34 +0100

    update coverage-processing lecteur mercator

--- python/coverage-processing/transform_mercator_to_ww3.py
+++ python/coverage-processing/transform_mercator_to_ww3.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     
     # Read file
     reader = MercatorReader('/home/retf/work/fieldsites/med-cruesim/modelling/mercator/grid/mercator_grid.nc',
-                            '/home/retf/work/fieldsites/med-cruesim/modelling/mercator/netcdf/mercator-grid2D-mars-2011.nc',
-                            '/home/retf/work/fieldsites/med-cruesim/modelling/mercator/netcdf/mercator-gridT-mars-2011.nc',
-                            '/home/retf/work/fieldsites/med-cruesim/modelling/mercator/netcdf/mercator-gridU-mars-2011.nc',
-                            '/home/retf/work/fieldsites/med-cruesim/modelling/mercator/netcdf/mercator-gridV-mars-2011.nc')      
+                            '/home/retf/work/fieldsites/med-cruesim/modelling/mercator/netcdf/mercator-grid2D-2012-2013.nc',
+                            '/home/retf/work/fieldsites/med-cruesim/modelling/mercator/netcdf/mercator-gridT-2012-2013.nc',
+                            '/home/retf/work/fieldsites/med-cruesim/modelling/mercator/netcdf/mercator-gridU-2012-2013.nc',
+                            '/home/retf/work/fieldsites/med-cruesim/modelling/mercator/netcdf/mercator-gridV-2012-2013.nc')      
         
     coverage = TimeLevelCoverage(reader); 
     
-    writer = WW3Writer('/home/retf/work/fieldsites/med-cruesim/modelling/waves/large-scale/simulation/cur/mercator-ww3_2.nc')  
+    writer = WW3Writer('/home/retf/work/fieldsites/med-cruesim/modelling/waves/med/config/cur/mercator-2012-2013.nc')  
     writer.write_axis(coverage);    
     writer.write_variable_wlv(coverage);
     writer.write_variable_current_at_level(coverage,0);   
