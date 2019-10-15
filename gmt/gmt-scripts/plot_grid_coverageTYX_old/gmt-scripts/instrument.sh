b9fb399990094e12f3a74c8ab6ad4c6d28ed3c21
Fabien Rétif <fabien.retif@zoho.com>
2019-10-15 14:25:44 +0200

    remove old

--- gmt/gmt-scripts/plot_grid_coverageTYX_old/gmt-scripts/instrument.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-psxy  $instrumentFile ${envelope} -Gred -W2p -J -P -O -K >> ${outfile}.ps
-
-echo "3.066668 42.704167" | psxy ${envelope} -Sc0.3c -Gorange -Wthin,black,solid -J -P -O -K -V >> ${outfile}.ps
-#psxy ${envelope} $instrumentFile/cigu-bouy.xy -S -Gred -Wfaint,black,solid -J -P -O -K >> ${outfile}.ps
-#psxy ${envelope} $instrumentFile/meteo.xy -S -Gorange -Wfaint,black,solid -J -P -O -K >> ${outfile}.ps
-#psxy ${envelope} $instrumentFile/tide.xy -S -Gpurple -Wfaint,black,solid -J -P -O -K >> ${outfile}.ps
-
